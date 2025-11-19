f4c454fb47d9e93b2b606200dd2527d8a0d0079b
DarylvdBerg <darylvdberg@gmail.com>
2025-11-19 10:54:16 +0100

    chore: Update implementation of empty or whitespace check

--- backend/internal/strings/utils.go
+++ backend/internal/strings/utils.go
@@ -1,5 +1,13 @@
 package strings
 
+import (
+	"regexp"
+	"strings"
+)
+
 func IsEmptyOrWhiteSpace(s string) bool {
-	return len(s) == 0 || s == " "
+	s = strings.ReplaceAll(s, " ", "")
+	empty := s == ""
+	whitespace := regexp.MustCompile(`\s+`).MatchString(s)
+	return empty || whitespace
 }
