a2f0fd30a95ce221a08834abb97d5dcaacbe13df
DarylvdBerg <darylvdberg@gmail.com>
2025-11-19 14:52:45 +0100

    test: add test for model conversion logic

--- backend/cmd/stock-o-matic-api/stock/model_test.go
+++ backend/cmd/stock-o-matic-api/stock/model_test.go
@@ -1,1 +1,47 @@
 package stock
+
+import (
+	"testing"
+
+	"github.com/DarylvdBerg/stock-o-matic/internal/database"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestToProto(t *testing.T) {
+	s := &stock{
+		Model: database.Model{
+			ID: 1,
+		},
+		Name: "Test Stock",
+	}
+
+	protoStock := s.toProto()
+
+	assert.Equal(t, s.ID, protoStock.Id)
+	assert.Equal(t, s.Name, protoStock.Name)
+}
+
+func TestToProtoSlice(t *testing.T) {
+	slice := []*stock{
+		{
+			Model: database.Model{
+				ID: 1,
+			},
+			Name: "Stock 1",
+		},
+		{
+			Model: database.Model{
+				ID: 2,
+			},
+			Name: "Stock 2",
+		},
+	}
+
+	protoStocks := toProtoSlice(slice)
+
+	assert.Len(t, protoStocks, 2)
+	for i, s := range slice {
+		assert.Equal(t, s.ID, protoStocks[i].Id)
+		assert.Equal(t, s.Name, protoStocks[i].Name)
+	}
+}
