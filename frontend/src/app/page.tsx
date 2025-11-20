3f7ae71428ce3bd64e9f0f42e4e060df41a2483c
DarylvdBerg <darylvdberg@gmail.com>
2025-11-20 17:55:37 +0100

    fix: wrap api call in try/catch and set response based on result

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,20 +2,21 @@ import { getClientConfig } from "@/config/client-config";
 import { StockClient } from "@/client/stock-client";
 import { Suspense } from "react";
 import { Overview } from "@/overview";
+import {GetStockResponse} from "@/proto/services/v1/stock_service_pb";
 
 export default function Home() {
 	const config = getClientConfig();
 	const client = new StockClient(config);
-
+	let response: Promise<GetStockResponse>
 	try {
-		const response = client.getStock();
-
-		return (
-			<Suspense fallback={<div>loading...</div>}>
-				<Overview stock={response} />
-			</Suspense>
-		);
+		response = client.getStock();
 	} catch {
-		return <div></div>;
+		response = Promise.reject()
 	}
+
+	return (
+		<Suspense fallback={<div>loading...</div>}>
+			<Overview stock={response} />
+		</Suspense>
+	);
 }
