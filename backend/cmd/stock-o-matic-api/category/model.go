1b99b5a874b5cd3cccc6f8ce97e520464a92bed2
DarylvdBerg <darylvdberg@gmail.com>
2025-11-20 09:10:09 +0100

    refactor: Change param to non-pointer to keep the conversion back to proto models simpler

--- backend/cmd/stock-o-matic-api/category/model.go
+++ backend/cmd/stock-o-matic-api/category/model.go
@@ -19,7 +19,7 @@ func (c *Category) toProto() *corev1.Category {
 }
 
 // ToProtoSlice converts a slice of Category database models to their protobuf representations.
-func ToProtoSlice(c []*Category) []*corev1.Category {
+func ToProtoSlice(c []Category) []*corev1.Category {
 	pCategories := make([]*corev1.Category, 0)
 	for _, dCategory := range c {
 		pCategories = append(pCategories, dCategory.toProto())
