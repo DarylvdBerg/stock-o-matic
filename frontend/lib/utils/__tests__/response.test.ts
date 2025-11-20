0040f9d79d79ce872aa9b0fde7294dc03fcaa3ff
DarylvdBerg <darylvdberg@gmail.com>
2025-11-20 17:05:08 +0100

    test: coverage for gestStockFromResponse

---
+++ frontend/lib/utils/__tests__/response.test.ts
@@ -0,0 +1,24 @@
+import { Stock } from "@/proto/core/v1/stock_pb";
+import { GetStockResponse } from "@/proto/services/v1/stock_service_pb";
+import { getStockFromResponse } from "../response";
+
+describe("getStockFromResponse", () => {
+	it("should return an array of stocks from response", () => {
+		const mockStock = {
+			id: 1,
+			name: "stock",
+			quantity: 20,
+		};
+
+		const mockResponse: GetStockResponse = {
+			$typeName: "proto.services.v1.GetStockResponse",
+			stocks: [mockStock as Stock],
+		};
+
+		const result = getStockFromResponse(mockResponse);
+
+		expect(result).toBeInstanceOf<Stock[]>;
+		expect(result).toHaveLength(1);
+		expect(result[0].name).toBe(mockStock.name);
+	});
+});
