9b3e532c9364a0d6eca2bef920da219e04bf2c1a
DarylvdBerg <darylvdberg@gmail.com>
2025-11-20 16:33:29 +0100

    chore: remove hooks since we're not going to need them for now

--- frontend/lib/hooks/use-stock.ts
+++
@@ -1,9 +0,0 @@
-import { useMemo } from "react";
-import { getClientConfig } from "@/config/client-config";
-import { StockClient } from "@/client/stock-client";
-
-export function useStockClient() {
-	return useMemo(() => {
-		return new StockClient(getClientConfig());
-	}, []);
-}
