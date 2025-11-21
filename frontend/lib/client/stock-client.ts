1fb9e38cc7985964661ce3d119172b9bc98e6929
DarylvdBerg <darylvdberg@gmail.com>
2025-11-21 14:03:58 +0100

    docs: update code docs

--- frontend/lib/client/stock-client.ts
+++ frontend/lib/client/stock-client.ts
@@ -3,8 +3,10 @@ import {
 	GetStockResponse,
 	StockService,
 } from "@/proto/services/v1/stock_service_pb";
-import { ConnectError, createClient } from "@connectrpc/connect";
-import { ClientError } from "./client-error";
+import { Code, ConnectError, createClient } from "@connectrpc/connect";
+import { RpcError } from "./rpc-error";
+import { doRpc } from "@/utils/client-helper";
+import { UnknownFailureRpcError } from "./errors";
 
 /**
  * Defines the Stock client for making stock related rpc calls.
@@ -29,18 +31,17 @@ export class StockClient {
 	 */
 	async getStock(): Promise<GetStockResponse> {
 		try {
-			// passing an empty request as we do not require any information.
 			const res = await this.client.getStock({});
 			return res;
 		} catch (error) {
 			if (error instanceof ConnectError) {
-				throw new ClientError(
-					`failed to fetch stock: ${error.message}`,
+				throw new RpcError(
+					`failed to execute rpc : ${error.message}`,
 					error.code,
 					error,
 				);
 			}
-			throw new ClientError("unknown error occured", undefined, error);
+			throw UnknownFailureRpcError;
 		}
 	}
 }
