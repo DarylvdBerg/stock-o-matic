7acdee7227b0ac32a4980e2be063bd088931c3de
DarylvdBerg <darylvdberg@gmail.com>
2025-11-28 16:15:02 +0100

    refactor: autocomplete to include multiple checkbox options
    The autocomplete field has been updated to a multiple value dropdown
    where we can select items through checkboxes. These values will now be
    rendered as chips within the categories field.

--- frontend/components/grid/grid.tsx
+++ frontend/components/grid/grid.tsx
@@ -8,6 +8,7 @@ import {
 	CardActions,
 	CardContent,
 	CardHeader,
+	Checkbox,
 	Chip,
 	Container,
 	InputAdornment,
@@ -22,6 +23,8 @@ import {
 import { Category, Stock } from "@/proto/core/v1/stock_pb";
 import SearchIcon from "@mui/icons-material/Search";
 import { GetCategoriesResponse } from "@/proto/services/v1/category_service_pb";
+import CheckBoxIcon from "@mui/icons-material/CheckBox";
+import CheckBoxOutlineBlankIcon from "@mui/icons-material/CheckBoxOutlineBlank";
 
 /**
  * Defines the properties for rendering our grid.
@@ -38,10 +41,15 @@ export function Grid({ stock, categories }: GridProps): JSX.Element {
 	const stockData = getStockFromResponse(use(stock));
 	const categoryData = getCategoriesFromResponse(use(categories));
 
-	const optionData = categoryData.map((c) => ({
-		id: c.id,
-		label: c.name,
-	}));
+	const optionData = categoryData
+		.filter((c) => c.name !== "")
+		.map((c) => ({
+			id: c.id,
+			label: c.name,
+		}));
+
+	const icon = <CheckBoxOutlineBlankIcon fontSize="small" />;
+	const checkedIcon = <CheckBoxIcon fontSize="small" />;
 
 	return (
 		<Container
@@ -63,8 +71,26 @@ export function Grid({ stock, categories }: GridProps): JSX.Element {
 					}}
 				/>
 				<Autocomplete
+					multiple
+					id="checkboxes-tags-demo"
 					options={optionData}
-					sx={{ width: 300 }}
+					disableCloseOnSelect
+					getOptionLabel={(option) => option.label}
+					renderOption={(props, option, { selected }) => {
+						const { key, ...optionProps } = props;
+						return (
+							<li key={key} {...optionProps}>
+								<Checkbox
+									icon={icon}
+									checkedIcon={checkedIcon}
+									style={{ marginRight: 8 }}
+									checked={selected}
+								/>
+								{option.label}
+							</li>
+						);
+					}}
+					style={{ width: 500 }}
 					renderInput={(params) => <TextField {...params} label="Categories" />}
 				/>
 			</Container>
